7095ef0979a7c07ce81490d5e8fde288767c3f21
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2023-08-03 12:43:48 +0700

    updated

--- my-app-vite/src/components/Section.jsx
+++ my-app-vite/src/components/Section.jsx
@@ -4,7 +4,9 @@ function Section() {
   const contents = [
     {
       headerTitle: 'Intensive Bootcamp ',
-      title: 'Programming Laravel Getting Started with Laravel 9',
+      title: 'Programming Laravel',
+      underTitle: '(Getting Started with Laravel 9)',
+      title2: 'Programming Laravel Getting Started with Laravel 9',
       content1: 'Batch: Setember 2023.',
       content2: 'Mentor: William Hartono, Farel Prayoga',
       price: 'Rp. 2.060.000',
@@ -12,7 +14,9 @@ function Section() {
     },
     {
       headerTitle: 'Intensive Bootcamp ',
-      title: 'Programming Laravel Getting Started with Laravel 9',
+      title: 'Programming Laravel',
+      underTitle: '(Getting Started with Laravel 9)',
+      title2: 'Programming Laravel Getting Started with Laravel 9',
       content1: 'Batch: Setember 2023.',
       content2: 'Mentor: William Hartono, Farel Prayoga',
       price: 'Rp. 2.060.000',
@@ -20,7 +24,9 @@ function Section() {
     },
     {
       headerTitle: 'Intensive Bootcamp ',
-      title: 'Programming Laravel Getting Started with Laravel 9',
+      title: 'Programming Laravel',
+      underTitle: '(Getting Started with Laravel 9)',
+      title2: 'Programming Laravel Getting Started with Laravel 9',
       content1: 'Batch: Setember 2023.',
       content2: 'Mentor: William Hartono, Farel Prayoga',
       price: 'Rp. 2.060.000',
@@ -28,7 +34,9 @@ function Section() {
     },
     {
       headerTitle: 'Intensive Bootcamp ',
-      title: 'Programming Laravel Getting Started with Laravel 9',
+      title: 'Programming Laravel',
+      underTitle: '(Getting Started with Laravel 9)',
+      title2: 'Programming Laravel Getting Started with Laravel 9',
       content1: 'Batch: Setember 2023.',
       content2: 'Mentor: William Hartono, Farel Prayoga',
       price: 'Rp. 2.060.000',
@@ -36,7 +44,9 @@ function Section() {
     },
     {
       headerTitle: 'Intensive Bootcamp ',
-      title: 'Programming Laravel Getting Started with Laravel 9',
+      title: 'Programming Laravel',
+      underTitle: '(Getting Started with Laravel 9)',
+      title2: 'Programming Laravel Getting Started with Laravel 9',
       content1: 'Batch: Setember 2023.',
       content2: 'Mentor: William Hartono, Farel Prayoga',
       price: 'Rp. 2.060.000',
@@ -44,7 +54,9 @@ function Section() {
     },
     {
       headerTitle: 'Intensive Bootcamp ',
-      title: 'Programming Laravel Getting Started with Laravel 9',
+      title: 'Programming Laravel',
+      underTitle: '(Getting Started with Laravel 9)',
+      title2: 'Programming Laravel Getting Started with Laravel 9',
       content1: 'Batch: Setember 2023.',
       content2: 'Mentor: William Hartono, Farel Prayoga',
       price: 'Rp. 2.060.000',
@@ -52,7 +64,9 @@ function Section() {
     },
     {
       headerTitle: 'Intensive Bootcamp ',
-      title: 'Programming Laravel Getting Started with Laravel 9',
+      title: 'Programming Laravel',
+      underTitle: '(Getting Started with Laravel 9)',
+      title2: 'Programming Laravel Getting Started with Laravel 9',
       content1: 'Batch: Setember 2023.',
       content2: 'Mentor: William Hartono, Farel Prayoga',
       price: 'Rp. 2.060.000',
@@ -60,7 +74,9 @@ function Section() {
     },
     {
       headerTitle: 'Intensive Bootcamp ',
-      title: 'Programming Laravel Getting Started with Laravel 9',
+      title: 'Programming Laravel',
+      underTitle: '(Getting Started with Laravel 9)',
+      title2: 'Programming Laravel Getting Started with Laravel 9',
       content1: 'Batch: Setember 2023.',
       content2: 'Mentor: William Hartono, Farel Prayoga',
       price: 'Rp. 2.060.000',
@@ -70,28 +86,31 @@ function Section() {
   ];
 
   return (
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {contents.map((item) => ( 
-          <div className="bg-white">
-            <section className="py-6 px-4 rounded-t-lg" key={item.title}>
-               <button className='text-left'>
-                <header className="bg-blue-900 text-white mx-0 py-2 px-4 rounded-t-lg text-right">
-                  <img src={cartoon} alt="cartoon" className="w-20 h-20" />
-                  <h1 className="text-[#FFCD29]">{item.headerTitle}</h1>
-                  <h2 className="text-white text-lg font-bold">{item.title}</h2>
-                  <h3 className="text-white">(getting started with Laravel 9)</h3>
-                </header>
-              </button>
-              <h1 className="text-lg font-bold indent-0">{item.title}</h1>
-              <p>{item.content1}</p>
-              <p>{item.content2}</p>
-              <h2 className="text-slate-400 line-through text-center">{item.price}</h2>
-              < h2 className="text-[#0ACF83] text-lg font-bold text-right">{item.discountPrice}</h2>
-            </section>
-          </div>
-        ))}
-      </div>
-    );
-  }
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {contents.map((item) => (
+        <section className="py-4 px-6 border rounded-md">
+  <div className="flex items-center space-x-4 bg-[#090c63] rounded-t-lg  h-[128px]">
+    <img src={cartoon} alt="cartoon" className="ml-2 w-16 h-16" />
+    <div className='flex flex-col'>
+      <h2 className="text-[#FFCD29] font-sherif font-bold">{item.headerTitle}</h2>
+      <h1 className="font-bold text-white font-sherif">{item.title}</h1>
+      <h3 className="text-white text-[10px]">{item.underTitle}</h3>
+    </div>
+  </div>
+  <div className='bg-white rounded-b-lg mg-2 flex flex-col justify-center overflow-hidden'>
+    <div className='p-4'>
+      <h1 className='text-lg font-bold'>{item.title2}</h1>
+      <h2>{item.content1}</h2>
+      <h2>{item.content2}</h2>
+      <h3 className='text-slate-400 line-through text-center'>{item.price}</h3>
+      <h3 className="text-[#0ACF83] text-lg font-bold text-right">{item.discountPrice}</h3>
+    </div>
+  </div>
+</section>
+
+      ))}
+    </div>
+  );
+}
   
 export default Section;
